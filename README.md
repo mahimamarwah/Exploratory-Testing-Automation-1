a0be4314c8259c57f14c59622518187b801407b3
mahimamarwah <129257415+mahimamarwah@users.noreply.github.com>
2023-12-20 17:59:13 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,29 @@
+# A Chrome Extension for Exploratory Testing
+
+A Chrome extension designed for making web exploratory testing easier
+
+**Features**
+
+ - Report bugs, ideas, notes and questions easily
+ - Take screenshots during the session. Keep focused 
+ - URL will be tracked automatically
+ - See session results in a report
+ - Export and import sessions
+ - Export session to JSON, CSV or HTML
+
+
+Install from [Chrome Store](https://chrome.google.com/webstore/detail/exploratory-testing-chrom/khigmghadjljgjpamimgjjmpmlbgmekj)
+
+
+**Annotate any thought while you test easily and take screenshots without changing context**
+
+<img src="./screenshots/new_Annotation.PNG" width="440">
+
+
+**View session results in a report**
+
+<img src="./screenshots/report.PNG" width="800">
+
+_PS: I'm not a web designer, so any help with web design or UX will be appreciated._
+
+twitter: @morvader
---
+++ background.js
@@ -0,0 +1,172 @@
+var session = new Session();
+
+chrome.extension.onMessage.addListener(function (request, sender, sendResponse) {
+    switch (request.type) {
+        case "addBug":
+            addAnnotation("Bug", request.name, request.imageURL);
+            break;
+        case "addIdea":
+            addAnnotation("Idea", request.name, request.imageURL);
+            break;
+        case "addNote":
+            addAnnotation("Note", request.name, request.imageURL);
+            break;
+        case "addQuestion":
+            addAnnotation("Question", request.name, request.imageURL);
+            break;
+        case "updateAnnotationName":
+            var AnnotationID = request.annotationID;
+            var newName = request.newName;
+
+            var annotations = session.getAnnotations();
+            var annotation = annotations[AnnotationID];
+            annotation.setName(newName);
+
+            break;
+        case "deleteAnnotation":
+            session.deleteAnnotation(request.annotationID);
+            break;
+        case "exportSessionCSV":
+            if (!exportSessionCSV())
+                sendResponse({
+                    status: "nothing to export"
+                });
+            break;
+        case "exportSessionJSon":
+            if (!exportSessionJSon())
+                sendResponse({
+                    status: "nothing to export"
+                });
+            break;
+        case "importSessionJSon":
+            var fileData = request.jSonSession;
+            if (!importSessionJSon(fileData))
+                sendResponse({
+                    status: "nothing to import"
+                });
+            break;
+        case "clearSession":
+            clearSession();
+            break;
+    }
+    sendResponse({
+        status: "ok"
+    });
+    return true;
+});
+
+function addAnnotation(type, name, imageURL) {
+
+    var currentUrl;
+    var now = Date.now();
+
+    if (session.getAnnotations().length == 0) this.startSession();
+
+    chrome.tabs.query({
+            currentWindow: true,
+            active: true
+        },
+        function (tabs) {
+
+            currentUrl = tabs[0].url;
+
+            //alert(currentUrl);
+            switch (type) {
+                case "Bug":
+                    var newBug = new Bug(name, currentUrl, now, imageURL);
+                    session.addBug(newBug);
+                    break;
+                case "Note":
+                    var newNote = new Note(name, currentUrl, now, imageURL);
+                    session.addNote(newNote);
+                    break;
+                case "Idea":
+                    var newIdea = new Idea(name, currentUrl, now, imageURL);
+                    session.addIdea(newIdea);
+                    break;
+                case "Question":
+                    var newQuestion = new Question(name, currentUrl, now, imageURL);
+                    session.addQuestion(newQuestion);
+                    break;
+            }
+        });
+}
+
+function startSession() {
+    var systemInfo = getSystemInfo();
+    session = new Session(Date.now(), systemInfo);
+};
+
+function clearSession() {
+    session.clearAnnotations();
+};
+
+
+function exportSessionCSV() {
+
+    if (session.getAnnotations().length == 0) return false;
+
+    var exportService = new ExportSessionCSV(session);
+    var csvData = exportService.getCSVData();
+
+    var browserInfo = session.getBrowserInfo();
+
+    var browserInfoString = browserInfo.browser + "_" + browserInfo.browserVersion;
+
+    //Take the timestamp of the first Annotation
+    var startDateTime = session.getStartDateTime().toString('yyyyMMdd_HHmm');
+
+    var fileName = "ExploratorySession_" + browserInfoString + "_" + startDateTime + ".csv";
+
+    var pom = document.createElement('a');
+    var blob = new Blob([csvData], {
+        type: 'text/csv;charset=utf-8;'
+    });
+    var url = URL.createObjectURL(blob);
+    pom.href = url;
+    pom.setAttribute('download', fileName);
+    pom.click();
+
+};
+
+function exportSessionJSon() {
+
+    if (session.getAnnotations().length == 0) return false;
+
+    debugger;
+    var exportJSonService = new JSonSessionService();
+    var jsonData = exportJSonService.getJSon(session);
+
+    var browserInfo = session.getBrowserInfo();
+
+    var browserInfoString = browserInfo.browser + "_" + browserInfo.browserVersion;
+
+    //Take the timestamp of the first Annotation
+    var startDateTime = session.getStartDateTime().toString('yyyyMMdd_HHmm');
+
+    var fileName = "ExploratorySession_" + browserInfoString + "_" + startDateTime + ".json";
+
+    var pom = document.createElement('a');
+    var blob = new Blob([jsonData], {
+        type: 'application/json'
+    });
+    var url = URL.createObjectURL(blob);
+    pom.href = url;
+    pom.setAttribute('download', fileName);
+    pom.click();
+
+};
+
+function importSessionJSon(JSonSessionData) {
+    debugger;
+    var exportJSonService = new JSonSessionService();
+    var importedSession = exportJSonService.getSession(JSonSessionData);
+
+    if (importedSession == null)
+        return false;
+
+    clearSession();
+    session = importedSession;
+
+    return true;
+};
